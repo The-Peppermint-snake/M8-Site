dc602f8bec75c8cdea22d0104988d8c12c9f9c34
The-Peppermint-Snake <brkovalik@gmail.com>
2026-03-04 13:31:10 -0500

    finsiehd up to page 224

--- js06a_txt.js
+++ js06a_txt.js
@@ -1,18 +1,38 @@
 "use strict";
-//Focus might not work (add auto focus in html file?)
+
 window.addEventListener("load", function() {
       let orderForm = document.forms.orderForm;
       let model = orderForm.elements.model;
       model.focus();
 
+      for (let i=0; i<orderForm.elements.length; i++) {
+            orderForm.elements[i].addEventListener("change", calcOrder);
+      }
+
       calcOrder();
       function calcOrder() {
             let mIndex = model.selectedIndex;
             let mValue = model.options[mIndex].value;
+
             let qIndex = orderForm.elements.qty.selectedIndex;
             let quantity = orderForm.elements.qty[qIndex].value;
+
             let modelCost = mValue*quantity;
-            orderForm.elements.modelCost.value = modelCost;
+            orderForm.elements.modelCost.value = modelCost.toLocaleString("en-Us", {style: "currency", currency: "USD"});
+
+            let planValue = document.querySelector('input[name="plan"]:checked').value;
+
+            let planCost = planValue*quantity;
+            orderForm.elements.planCost.value = planCost.toLocaleString("en-Us", {style: "currency", currency: "USD"});
+
+            let subtotal = modelCost + planCost;
+            orderForm.elements.subtotal.value = subtotal.toLocaleString("en-Us", {style: "currency", currency: "USD"});
+
+            let salesTax = subtotal *  0.05;
+            orderForm.elements.salesTax.value = salesTax.toLocaleString("en-Us", {style: "currency", currency: "USD"});
+
+            let totalCost = subtotal+salesTax;
+            orderForm.elements.totalCost.value = totalCost.toLocaleString("en-Us", {style: "currency", currency: "USD"});
       }
 });
 

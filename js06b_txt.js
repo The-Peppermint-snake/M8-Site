3fc8686fe94937f06899ed12202e21ee4f499e3b
The-Peppermint-Snake <brkovalik@gmail.com>
2026-03-04 14:09:20 -0500

    good just have to do page 241

--- js06b_txt.js
+++ js06b_txt.js
@@ -1,32 +1,74 @@
 "use strict";
-/*    JavaScript 7th Edition
-      Chapter 6
-      Chapter case
 
-      Order Form Code
-      Author: 
-      Date:   
-
-      Filename: js06b.js
- */
+let subButton = document.getElementById("subButton");
+subButton.addEventListener("click", validateName);
+subButton.addEventListener("click", validateCard);
+subButton.addEventListener("click", validateNumber);
+subButton.addEventListener("click", validateMonth);
+subButton.addEventListener("click", validateYear);
+subButton.addEventListener("click", validateCVC);
 
 
+function validateName() {
+   let cardName = document.getElementById("cardName");
+   if (cardName.validity.valueMissing) {
+      cardName.setCustomValidity("Enter your name as it appears on the card");
+   } else {
+      cardName.setCustomValidity("");
+   }
+}
 
+function validateCard() {
+   let card = document.forms.payment.elements.credit[0];
+   if (card.validity.valueMissing) {
+      card.setCustomValidity("Select your credit card");
+   } else {
+      card.setCustomValidity("");
+   }
+}
 
+function validateNumber() {
+   let cNum = document.getElementById("cardNumber")
+   if (cNum.validity.valueMissing) {
+      card.setCustomValidity("Enter your card number");
+   } else if (cNum.validity.patternMismatch) {
+      cNum.setCustomValidity("Enter a valid card number");
+   } else {
+      card.setCustomValidity("");
+   }
+}
 
+function validateMonth() {
+   let month = document.getElementById("expMonth");
+   if (month.validity.valueMissing) {
+      month.setCustomValidity("Select the expiration month");
+   } else {
+      month.setCustomValidity("");
+   }
+}
 
+function validateYear() {
+   let year = document.getElementById("expYear");
+   if (year.selectedIndex === 0) {
+      year.setCustomValidity("Select the expiration year");
+   } else {
+      year.setCustomValidity("");
+   }
+}
 
-
-
-
-
-
-
-
-
-
-
-
+function validateCVC() {
+   let card = document.querySelector('input[name="credit"]:checked').value;
+   let cvc = document.getElementById("cvc");
+   if (cvc.validity.valueMissing) {
+      cvc.setCustomValidity("Enter your cvc number");
+   } else if ((card === "amex") && !(/^\d{4}$/.test(cvc.value))) {
+      cvc.setCustomValidity("Enter a 4-digit number");
+   } else if ((card !== "amex") && !(/^\d{3}$/.test(cvc.value))) {
+      cvc.setCustomValidity("Enter a 3-digit number");
+   } else {
+      cvc.setCustomValidity("");
+   }
+}
 
 /* ------- Luhn Algorithm used for Validating Credit Card Numbers   ----- */
 
